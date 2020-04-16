6e6b9be3f900ea9311a7f8c11cf4282f6233ab84
martamxd <martamxd@wp.pl>
2020-04-16 13:21:34 +0200

    python manage.py shell

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import Choice, Question, View
 
-from .models import Question
+@admin.register(View)
+class ViewAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
